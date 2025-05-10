2ab9481a5d4f3e546b03c0efd62e5fff0c0b1576
yasasdev <yasaslekamge@gmail.com>
2025-05-10 19:22:38 +0530

    ShopContext create

--- src/final-project-HND-frontend/src/main.jsx
+++ src/final-project-HND-frontend/src/main.jsx
@@ -2,9 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.scss';
+import Shop from './pages/Shop';
+import ShopContextProvider from './Context/ShopContext';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    
+    <ShopContextProvider>
+      <App />
+    </ShopContextProvider>
+    
   </React.StrictMode>,
 );
