13c3d770f35615c89616c6f667efc03c07f95160
yasasdev <yasaslekamge@gmail.com>
2025-05-10 17:33:25 +0530

    Newsletter file update with email feature

--- src/final-project-HND-frontend/src/components/NewsLetter/NewsLetter.jsx
+++ src/final-project-HND-frontend/src/components/NewsLetter/NewsLetter.jsx
@@ -7,7 +7,7 @@ const NewsLetter = () => {
         <h1>Get Exclusve Offers On Your Email</h1>
         <p>Subscribe to our newsletter and stay updated</p>
         <div>
-            <input type="email" placeholder='Your Email id' />
+            <input type="email" placeholder='Enter Your Email' />
             <button>Subscribe</button>
         </div>
     </div>
