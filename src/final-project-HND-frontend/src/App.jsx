63f6972465dab6c48dd86b28a953cafc051939da
yasasdev <yasaslekamge@gmail.com>
2025-05-12 12:09:04 +0530

    ShopCategory file update

--- src/final-project-HND-frontend/src/App.jsx
+++ src/final-project-HND-frontend/src/App.jsx
@@ -8,6 +8,9 @@ import ShopCategory from './pages/ShopCategory';
 import LoginSignUp from './pages/LoginSignUp';
 import Cart from './pages/Cart';
 import Footer from './components/Footer/Footer';
+import men_banner from './components/Assets/banner_mens.png'
+import women_banner from './components/Assets/banner_women.png'
+import kids_banner from './components/Assets/banner_kids.png'
 
 function App() {
 
@@ -18,9 +21,9 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Shop/>} />
-        <Route path="/men" element={<ShopCategory category="men"/>} />
-        <Route path="/women" element={<ShopCategory category="women"/>} />
-        <Route path="/kids" element={<ShopCategory category="kid"/>} />
+        <Route path="/men" element={<ShopCategory banner={men_banner} category="men"/>} />
+        <Route path="/women" element={<ShopCategory banner={women_banner} category="women"/>} />
+        <Route path="/kids" element={<ShopCategory banner={kids_banner} category="kid"/>} />
         <Route path="/product" element={<Product/>}>
           <Route path=":productId" element={<Product/>} />
         </Route>
