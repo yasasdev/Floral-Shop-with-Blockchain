b2bd1d788dbb9953aba8ded4a7efb7affa81d1e2
yasasdev <yasaslekamge@gmail.com>
2025-05-18 17:53:03 +0530

    Login page update with css

--- src/final-project-HND-frontend/src/pages/LoginSignUp.jsx
+++ src/final-project-HND-frontend/src/pages/LoginSignUp.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import './CSS/LoginSignup.css'
 
 const LoginSignUp = () => {
   return (
-    <div>LoginSignUp</div>
+    <div className='loginSignup'>
+      <div className="loginSignup-container">
+        <h1>Sign Up</h1>
+        <div className="loginSignup-fields">
+          <input type="text" placeholder='Your Name' />
+          <input type="email" placeholder='Email Address' />
+          <input type="password" placeholder='Password' />
+        </div>
+        <button>Continue</button>
+        <p className="loginSignup-login">
+          Already have an account? <span>Login here</span>
+          <div className="loginSignup-agree">
+            <input type="checkbox" name='' id='' />
+            <p>By continuing, I agree to the terms of use & privacy policy.</p>
+          </div>
+        </p>
+      </div>
+    </div>
   )
 }
 
