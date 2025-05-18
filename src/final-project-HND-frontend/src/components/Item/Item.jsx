4d84c2ab6070d8a59a1f3b2747892e6d82cd4208
yasasdev <yasaslekamge@gmail.com>
2025-05-19 00:55:42 +0530

    Update the scroll to top function when a user clicks on a related item.

--- src/final-project-HND-frontend/src/components/Item/Item.jsx
+++ src/final-project-HND-frontend/src/components/Item/Item.jsx
@@ -3,9 +3,16 @@ import { Link } from 'react-router-dom'
 import './Item.css'
 
 const Item = (props) => {
+  const handleClick = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
+  }
+
   return (
     <div className='item'>
-        <Link to={`/product/${props.id}`}><img src={props.image} alt="" /></Link> 
+        <Link to={`/product/${props.id}`}><img onClick={handleClick} src={props.image} alt="" /></Link> 
         <p>{props.name}</p>
         <div className="item-prices">
             <div className="item-price-new">
