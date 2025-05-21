7bae7fb8d6144220ada1542f65e20bbfc12c34d0
yasasdev <yasaslekamge@gmail.com>
2025-05-21 17:03:59 +0530

    Cart items adding part updated

--- src/final-project-HND-frontend/src/components/ProductDisplay/ProductDisplay.jsx
+++ src/final-project-HND-frontend/src/components/ProductDisplay/ProductDisplay.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import './ProductDisplay.css'
 import star_icon from '../Assets/star_icon.png'
 import star_dull_icon from '../Assets/star_dull_icon.png'
+import { ShopContext } from '../../Context/ShopContext';
+
 
 const ProductDisplay = (props) => {
+
     const {product} = props;
+    const {addToCart} = useContext(ShopContext);
+
   return (
     <div className='product-display'>
         <div className="product-display-left">
@@ -45,7 +50,7 @@ const ProductDisplay = (props) => {
                     <div>XXL</div>
                 </div>
             </div>
-            <button>ADD TO CART</button>
+            <button onClick={()=>{addToCart(product.id)}}>ADD TO CART</button>
             <p className='product-display-right-category'><span>Category :</span>Women, T-Shirt, Crop Tops</p>
             <p className='product-display-right-category'><span>Tags :</span>Modern, Latest</p>
         </div>
