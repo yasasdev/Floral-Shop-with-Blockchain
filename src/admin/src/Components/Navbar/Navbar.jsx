7a1cfafe8473c2b43603e164e8d221df117f263d
yasasdev <yasaslekamge@gmail.com>
2025-05-24 19:19:00 +0530

    Navbar updated with logo and profile SVGs

--- src/admin/src/Components/Navbar/Navbar.jsx
+++ src/admin/src/Components/Navbar/Navbar.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './Navbar.css'
-import navlogo from '../../assets/nav-logo.svg'
-import navProfile from '../../assets/nav-profile.svg'
+import navlogo from '../../assets/Admin_Assets/nav-logo.svg'
+import navProfile from '../../assets/Admin_Assets/nav-profile.svg'
 
 const Navbar = () => {
   return (
