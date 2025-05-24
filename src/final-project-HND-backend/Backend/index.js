f552f6b45b1c369404a9a6b54bbea0400876dada
yasasdev <yasaslekamge@gmail.com>
2025-05-24 14:27:23 +0530

    Uploading images part initialized.

--- src/final-project-HND-backend/Backend/index.js
+++ src/final-project-HND-backend/Backend/index.js
@@ -11,14 +11,31 @@ app.use(express.json());
 app.use(cors());
 
 // Connect connection with MongoDB
-// Username - yasasDev
-// Password - yasasDev#123
 mongoose.connect("mongodb+srv://yasasDev:yasasDev123@cluster0.e6cs3fm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
 
 // API Creation
 
 app.get("/", (req,res) => {
     res.send("Express App is Running")
+})
+
+// Image Storage Engine
+const storage = multer.diskStorage({
+    destination: './upload/images',
+    filename: (req, file, cb) => {
+        return cb(null, `${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`);
+    }
+});
+
+const upload = multer({storage:storage})
+
+// Creating upload endpoint for images 
+app.use('/images', express.static(path.join('upload/images')));
+app.post("/upload", upload.single('product'), (req, res) => {
+    res.json({
+        success: 1,
+        image_url: `http://localhost:${port}/images/${req.file.filename}`
+    })
 })
 
 app.listen(port, (err) => {
