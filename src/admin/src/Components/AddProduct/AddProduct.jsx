be9634b9beaad5db5efce88967eaa38dfb788bf6
yasasdev <yasaslekamge@gmail.com>
2025-05-26 17:04:23 +0530

    Multiple files update. Created add product, list product and sidebar component. Update the admin file and update the css files for those rekated files.

---
+++ src/admin/src/Components/AddProduct/AddProduct.jsx
@@ -0,0 +1,64 @@
+import React, { useState } from 'react'
+import './AddProduct.css'
+import upload_area from '../../assets/Admin_Assets/upload_area.svg'
+
+const AddProduct = () => {
+
+    const [image, setImage] = useState(false);
+    const [productDetails, setProductDetails] = useState({
+        name:"",
+        image:"",
+        category:"woemn",
+        new_price:"",
+        old_price:""
+    })
+
+    const imageHandler = (e) => {
+        setImage(e.target.files[0]);
+    }
+
+    const changeHandler = (e) => {
+        setProductDetails({...productDetails,[e.target.name]:e.target.value});
+    }
+
+    const addProduct = async () => {
+        // console.log(productDetails);
+        
+    }
+
+  return (
+    <div className='add-product'>
+        <div className="addproduct-itemfield">
+            <p>Product Title</p>
+            <input value={productDetails.name} onChange={changeHandler} type="text" name='name' placeholder='Type Here...' />
+        </div>
+        <div className="addproduct-price">
+            <div className="addproduct-itemfield">
+                <p>Price</p>
+                <input value={productDetails.old_price} onChange={changeHandler} type="text" name='old_price' placeholder='Type Here...' />
+            </div>
+            <div className="addproduct-itemfield">
+                <p>Offer Price</p>
+                <input value={productDetails.new_price} onChange={changeHandler} type="text" name='new_price' placeholder='Type Here...' />
+            </div>
+        </div>
+        <div className="addproduct-itemfield">
+            <p>Product Category</p>
+            <select value={productDetails.category} onChange={changeHandler} name="category" className='add-product-selector'>
+                <option value="women">Women</option>
+                <option value="men">Men</option>
+                <option value="kid">Kid</option>
+            </select>
+        </div>
+        <div className="addproduct-itemfield">
+            <label htmlFor="file-input">
+                <img src={image?URL.createObjectURL(image):upload_area} className='addproduct-thumnail-image' alt="" />
+            </label>
+            <input onChange={imageHandler} type="file" name='image' id='file-input' hidden />
+        </div>
+        <button onClick={()=>{addProduct()}} className='addproduct-btn'>ADD</button>
+    </div>
+  )
+}
+
+export default AddProduct
