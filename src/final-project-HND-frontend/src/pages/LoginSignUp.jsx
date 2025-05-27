fa2e94bee3d4a6a5da6ac8db86c4b25d8ada9fa6
yasasdev <yasaslekamge@gmail.com>
2025-05-27 22:12:12 +0530

    Update the function of changing the forms between login and sign up

--- src/final-project-HND-frontend/src/pages/LoginSignUp.jsx
+++ src/final-project-HND-frontend/src/pages/LoginSignUp.jsx
@@ -1,24 +1,27 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './CSS/LoginSignup.css'
 
 const LoginSignUp = () => {
+
+  const [state,setstate] = useState("Login");
+
   return (
     <div className='loginSignup'>
       <div className="loginSignup-container">
-        <h1>Sign Up</h1>
+        <h1>{state}</h1>
         <div className="loginSignup-fields">
-          <input type="text" placeholder='Your Name' />
+          {state==="Sign Up"?<input type="text" placeholder='Your Name' />:<></>}
           <input type="email" placeholder='Email Address' />
           <input type="password" placeholder='Password' />
         </div>
         <button>Continue</button>
-        <p className="loginSignup-login">
-          Already have an account? <span>Login here</span>
+        {state==="Sign Up"
+        ?<p className="loginSignup-login">Already have an account? <span onClick={()=>{setstate("Login")}}>Login here</span></p>
+        :<p className="loginSignup-login">Create an account? <span onClick={()=>{setstate("Sign Up")}}>Click here</span></p>}        
           <div className="loginSignup-agree">
             <input type="checkbox" name='' id='' />
             <p>By continuing, I agree to the terms of use & privacy policy.</p>
           </div>
-        </p>
       </div>
     </div>
   )
