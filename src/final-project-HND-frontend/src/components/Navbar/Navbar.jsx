d063edfa6c55414493cafad730022a2da9f50401
yasasdev <yasaslekamge@gmail.com>
2025-05-29 11:31:54 +0530

    Installed fortawesome npm packages and added a wallet icon to the navbar and added some css to it

--- src/final-project-HND-frontend/src/components/Navbar/Navbar.jsx
+++ src/final-project-HND-frontend/src/components/Navbar/Navbar.jsx
@@ -4,6 +4,8 @@ import logo from '../Assets/logo.png'
 import cart_icon from '../Assets/cart_icon.png'
 import { Link } from 'react-router-dom';
 import { ShopContext } from '../../Context/ShopContext';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faWallet } from '@fortawesome/free-solid-svg-icons';
 
 const Navbar = () => {
 
@@ -26,6 +28,7 @@ const Navbar = () => {
         {localStorage.getItem('auth-token')
         ?<button onClick={()=>{localStorage.removeItem('auth-token');window.location.replace('/')}}>Logout</button>
         :<Link to='/login'><button>Login</button></Link>}        
+        <Link to='/payment'><FontAwesomeIcon icon={faWallet} className="wallet-icon" /></Link>
         <Link to='/cart'><img src={cart_icon} alt="" /></Link>
         <div className="nav-cart-count">{getTotalCartItems()}</div>
       </div>
